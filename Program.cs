627002a313a92bbdbb7b29170d286949c1865025
jb <jb@DESKTOP-SER8NB8>
2023-02-09 16:45:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/* Jacob Broerman
+ Feb 9 2023
+ Description: Create a C# console application that computes the first 25 numbers of the Fibonacci sequence with no user input.
+*/
+
+using System;
+
+namespace Deliv4Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25];
+
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < 25; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Fibonacci number ({0}) = {1}", i, fibonacci[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
